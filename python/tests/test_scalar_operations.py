86525accab99cf552bc553a07e970b1dd3874626
Piotr Tchir <piotr.t.tchir@gmail.com>
2025-01-17 17:24:45 +0200

    configure python tests

---
+++ python/tests/test_scalar_operations.py
@@ -0,0 +1,14 @@
+from scalar_operations import add, subtract, multiply, divide
+
+def test_add():
+    assert add(1, 2) == 3
+
+def test_subtract():
+    assert subtract(1, 2) == -1
+
+def test_multiply():
+    assert multiply(1, 2) == 2
+
+def test_divide():
+    assert divide(1, 2) == 0.5
+
